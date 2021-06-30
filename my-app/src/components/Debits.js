5fc221f6a8f44bf4957d50aa182eb5ce3502be97
saimantmg01 <saimantmg01@gmail.com>
2021-06-29 20:38:43 -0400

    added css for components and nav component

--- my-app/src/components/Debits.js
+++ my-app/src/components/Debits.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AccountBalance from './AccountBalance';
 import { Link } from 'react-router-dom';
+import '../css/Debits.css';
 
 class Debits extends Component {
     constructor(props) {
@@ -64,6 +65,27 @@ class Debits extends Component {
 
     }
 
+    TableHeader = () => {
+        let header = ["#","AMOUNT","DESCRIPTION","DATE"];
+        return header.map((key, index) => {
+            return <th key={index}>{key}</th>
+        })
+    }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+
+    TableData = () =>{
+        return this.props.debits.map((debits, index) => {
+            return (
+              //  <li key={index}>Amount: {debits.amount} &nbsp;&nbsp;&nbsp; Description: {debits.description} &nbsp;&nbsp; &nbsp; Date: {debits.date}</li>
+                <tr key={index}>
+                    <td>{index}</td>
+                    <td>{debits.amount}</td>
+                    <td>{debits.description}</td>
+                    <td>{debits.date}</td>
+                </tr>    
+            )
+        })
+    }
 
 
     render() {
@@ -74,30 +96,27 @@ class Debits extends Component {
                     <h1>Debits</h1>
                 </div>
                 <div>
-                    <ul>
-                        {
-                            this.props.debits.map((debits, index) => {
-                                return (
-                                    <li key={index}>Amount: {debits.amount} &nbsp;&nbsp;&nbsp; Description: {debits.description} &nbsp;&nbsp; &nbsp; Date: {debits.date}</li>
-                                )
-                            })
-                        }
-
-                    </ul>
-
-                </div>
-                <div>
+                <Link to="/">Back to Home</Link>
+                <AccountBalance accountBalance={this.props.accountBalance} />
                     <form onSubmit={this.handleSubmit}>
-                        &nbsp;&nbsp;Amount:&nbsp;&nbsp;
+                        &nbsp;&nbsp;<strong>Amount:</strong>&nbsp;&nbsp;
                         <input type="text" id="Amount" name='amount' value={this.state.theamount} onChange={this.handleChangeAmount} />
-                        &nbsp;&nbsp; Description:&nbsp;&nbsp;
+                        &nbsp;&nbsp;<strong>Description:</strong>&nbsp;&nbsp;
                         <input type="text" id="Description" name='description' value={this.state.theDesc} onChange={this.handleChangeDescription} />&nbsp;&nbsp;
-                        <input type="submit" value="Submit" />
+                        <input className= "buttn" type="submit" value="Submit" />
                     </form>
 
                 </div>
-                <Link to="/">Home</Link>
-                <AccountBalance accountBalance={this.props.accountBalance} />
+                <div>
+                <table id='debits_table'>
+                    <thead>
+                        <tr>{this.TableHeader()}</tr>
+                    </thead>
+                    <tbody>
+                        {this.TableData()}
+                    </tbody>
+                </table>
+                </div>
             </div >
         );
     }
