0aaa47d46f3487db9132fb031d181ae407e8c199
Fourcan Abdullah <fourcanabdullah@gmail.com>
2021-07-01 15:19:50 -0400

    added commits fixed minor string issue

--- my-app/src/components/Debits.js
+++ my-app/src/components/Debits.js
@@ -5,26 +5,26 @@ import '../css/Debits.css';
 
 class Debits extends Component {
     constructor(props) {
-        super(props); 
-            this.state = {
-                theamount: '',
-                theDesc: '',
+        super(props);
+        this.state = {
+            theamount: '',
+            theDesc: '',
 
-            }
+        }
 
-            this.handleChangeAmount = this.handleChangeAmount.bind(this);
-            this.handleChangeDescription = this.handleChangeDescription.bind(this);
-            this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeAmount = this.handleChangeAmount.bind(this);
+        this.handleChangeDescription = this.handleChangeDescription.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
 
 
 
 
-    handleChangeAmount(event) {
+    handleChangeAmount(event) {         //handle change for amount input
 
-        let input = event.target.value;
-        if ((isNaN(input))) {
+        let input = event.target.value;         //store value into input
+        if ((isNaN(input))) {                   //if the input is not a number alert user
             alert("Please Input a Number")
         }
 
@@ -32,7 +32,7 @@ class Debits extends Component {
         else {
             this.setState({
 
-                theamount: input,
+                theamount: input,           //set the state
 
 
 
@@ -41,47 +41,48 @@ class Debits extends Component {
 
 
     }
-    handleChangeDescription(event) {
+    handleChangeDescription(event) {        //handle change for description input
+
 
         this.setState({
 
-            theDesc: event.target.value,
+            theDesc: event.target.value,        //set the state
 
 
         })
 
     }
-    handleSubmit(event) {
-        event.preventDefault();
-        let date = Date().toLocaleString()
-        let fixer = Number(this.state.theamount).toFixed(2)
-        let addedData = {
+    handleSubmit(event) {                               //once input button is pressed
+        event.preventDefault();                         //make sure page doesnt refresh
+        let date = Date().toLocaleString()                  //get the current date and tiem
+        let fixer = Number(this.state.theamount).toFixed(2)     //set the amount to 2 decimal       
+        let addedData = {                               //create an object
             amount: fixer,
             description: this.state.theDesc,
             date: date,
         }
-        this.props.addDebit(addedData);
+        this.props.addDebit(addedData);             //make the object the paramenter
 
     }
 
     TableHeader = () => {
-        let header = ["#","AMOUNT","DESCRIPTION","DATE"];
+        let header = ["#", "AMOUNT", "DESCRIPTION", "DATE"];
         return header.map((key, index) => {
             return <th key={index}>{key}</th>
         })
     }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
 
-    TableData = () =>{
+
+    TableData = () => {
         return this.props.debits.map((debits, index) => {
             return (
-              //  <li key={index}>Amount: {debits.amount} &nbsp;&nbsp;&nbsp; Description: {debits.description} &nbsp;&nbsp; &nbsp; Date: {debits.date}</li>
+                //  <li key={index}>Amount: {debits.amount} &nbsp;&nbsp;&nbsp; Description: {debits.description} &nbsp;&nbsp; &nbsp; Date: {debits.date}</li>
                 <tr key={index}>
                     <td>{index}</td>
                     <td>{debits.amount}</td>
                     <td>{debits.description}</td>
                     <td>{debits.date}</td>
-                </tr>    
+                </tr>
             )
         })
     }
@@ -95,26 +96,26 @@ class Debits extends Component {
                     <h1>Debits</h1>
                 </div>
                 <div>
-                <Link to="/">Back to Home</Link>
-                <AccountBalance accountBalance={this.props.accountBalance} />
+                    <Link to="/">Back to Home</Link>
+                    <AccountBalance accountBalance={Number(Number(this.props.accountBalance).toFixed(2))} />
                     <form onSubmit={this.handleSubmit}>
                         &nbsp;&nbsp;<strong>Amount:</strong>&nbsp;&nbsp;
                         <input type="text" id="Amount" name='amount' value={this.state.theamount} onChange={this.handleChangeAmount} />
                         &nbsp;&nbsp;<strong>Description:</strong>&nbsp;&nbsp;
                         <input type="text" id="Description" name='description' value={this.state.theDesc} onChange={this.handleChangeDescription} />&nbsp;&nbsp;
-                        <input className= "buttn" type="submit" value="Submit" />
+                        <input className="buttn" type="submit" value="Submit" />
                     </form>
 
                 </div>
                 <div>
-                <table id='debits_table'>
-                    <thead>
-                        <tr>{this.TableHeader()}</tr>
-                    </thead>
-                    <tbody>
-                        {this.TableData()}
-                    </tbody>
-                </table>
+                    <table id='debits_table'>
+                        <thead>
+                            <tr>{this.TableHeader()}</tr>
+                        </thead>
+                        <tbody>
+                            {this.TableData()}
+                        </tbody>
+                    </table>
                 </div>
             </div >
         );
